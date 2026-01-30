9070f534abaf3813c92bf660ae7a7071dcab805e
Helix1000 <111099337+Helix1000@users.noreply.github.com>
2026-01-30 11:51:35 +0100

    Shoot Tank

--- Assets/Scrips/Bullet.cs
+++ Assets/Scrips/Bullet.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -5,16 +6,33 @@ public class Bullet : MonoBehaviour
     private Rigidbody2D rb;
 
     [SerializeField] float speed = 30f;
+    [SerializeField] int baunces = 1;
 
     private Vector2 direction;
-    public void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    public void Bullets(Vector2 direction)
+    public void Shoot(Vector2 direction)
     {
         this.direction = direction;
 
-        rb.linearVelocity = this.direction * speed; 
+        rb.linearVelocity = this.direction * speed;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        baunces--;
+        if (baunces < 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        var firstContact = collision.contacts[0];
+        Vector2 newVelocity = Vector2.Reflect(direction.normalized, firstContact.normal);
+        Shoot(newVelocity.normalized);
+    }
+    private void FixedUpdate()
+    {
+        transform.LookAt(direction);
     }
 }
--- Assets/Scrips/PlayerShooting.cs
+++ Assets/Scrips/PlayerShooting.cs
@@ -3,22 +3,35 @@ using UnityEngine.InputSystem;
 
 public class PlayerShooting : MonoBehaviour
 {
-    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Bullet bulletPrefab;
     [SerializeField] Transform bulletSpawnPos;
-
-    
+   
 
     InputAction shootAction;
+
+    Vector2 moving;
     private void Awake()
     {
+
         shootAction = InputSystem.actions.FindAction("Shoot");
+
     }
 
     private void Update()
     {
         if (shootAction.WasPressedThisFrame())
         {
-            //Vector2 direction = 
+           
+            Vector2 direction = moving;
+            if (direction.sqrMagnitude < 0.01f) direction = -transform.up;
+            {
+                float angulo = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                Quaternion rotacion = Quaternion.Euler(0, 0, angulo);
+
+                Bullet newBullet = Instantiate(bulletPrefab, bulletSpawnPos.position, rotacion);
+                newBullet.Shoot(direction.normalized);
+
+            }
         }
     }
 }
