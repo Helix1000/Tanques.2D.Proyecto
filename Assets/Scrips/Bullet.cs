3c605395f22ab6d3cebf1400d4d1409270541ed1
Helix1000 <111099337+Helix1000@users.noreply.github.com>
2026-02-01 21:58:31 +0100

    Sprite correct direction

--- Assets/Scrips/Bullet.cs
+++ Assets/Scrips/Bullet.cs
@@ -33,6 +33,7 @@ public class Bullet : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        transform.LookAt(direction);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
