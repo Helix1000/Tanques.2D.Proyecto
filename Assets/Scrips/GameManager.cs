afce40398b93ac97002d228fe20e0fd8d9dc0ca6
Helix1000 <111099337+Helix1000@users.noreply.github.com>
2026-02-06 10:57:03 +0100

    Rondas-tankes

--- Assets/Scrips/GameManager.cs
+++ Assets/Scrips/GameManager.cs
@@ -1,35 +1,90 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-       
+
+    public GameObject button;
+
     int puntos = 0;
+    int puntos2 = 0;
+
+    [Header("Player1")]
     public GameObject p1, punto1, punto2, punto3;
+
+    [Header("Player2")]
+    public GameObject p2, punto4, punto5, punto6;
 
     private void Awake()
     {
-        Instance = this;
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("bala"))
+
+
+        if (Instance != null)
         {
-            puntos++;
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
     }
-    private void FixedUpdate()
+
+    public void GanarRonda()
     {
+        puntos++;
         if (puntos == 1)
         {
-            p1.SetActive(true);
             punto1.SetActive(true);
+            p1.SetActive(true);
+            button.SetActive(true);
         }
+        else if (puntos == 2)
+        {
+            punto2.SetActive(true);
+            p1.SetActive(true);
+            button.SetActive(true);
+        }
+        else if (puntos == 3)
+        {
+            punto3.SetActive(true);
+            p1.SetActive(true);
+            button.SetActive(true);
+        }
+
+
     }
-    //IEnumerator Rondas(Collider2D collision)
-    //{
+    public void GanarRonda2()
+    {
+        puntos2++;
+        if (puntos == 1)
+        {
+            punto4.SetActive(true);
+            p2.SetActive(true);
+            button.SetActive(true);
+        }
+        else if (puntos == 2)
+        {
+            punto5.SetActive(true);
+            p2.SetActive(true);
+            button.SetActive(true);
+        }
+        else if (puntos == 3)
+        {
+            punto6.SetActive(true);
+            p2.SetActive(true);
+            button.SetActive(true);
+        }
 
-    //}
 
+    }
+
+    public void Reseteo()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
+
--- Assets/Scrips/PlayerDestuction.cs
+++ Assets/Scrips/PlayerDestuction.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class PlayerDestuction : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         
+        if (collision.gameObject.CompareTag("Bala"))
+        {
+
+            GameManager.Instance.GanarRonda();
+
+            Destroy(collision.gameObject);
+
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Bala2"))
+        {
+
+            GameManager.Instance.GanarRonda2();
+
+            Destroy(collision.gameObject);
+
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
